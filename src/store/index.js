3d795ccf5b4ce7d967bf1c33a82725cc74c75bc2
Shashimii <besasamreginald36@gmail.com>
2025-01-08 00:41:28 +0800

    + dummy data

--- src/store/index.js
+++ src/store/index.js
@@ -2,8 +2,28 @@ import { createStore } from 'vuex'
 
 export default createStore({
   state: {
+    duties: [
+      {id: 1, duty: '1. Rendering of Legal Opinion and other Legal Services'},
+      {id: 1, duty: '1.1 Rendering of Legal Opinion'},
+      {id: 1, duty: '1.2 Review of Contracts/MOA/ MOU and other Agreements'},
+      {id: 1, duty: '1.3 Evaluation of Investigation Reports (Forestry Cases)'},
+      {id: 1, duty: '1.4 Draft Correspondence to Quick Response Actions-8888 Complaints, Ombudsman, etc.'},
+    ],
+    
+    officers: [
+      {id: 1, name: 'Officer 1'},
+      {id: 2, name: 'Officer 2'},
+      {id: 3, name: 'Officer 3'},
+      {id: 4, name: 'Officer 4'},
+      {id: 5, name: 'Officer 5'},
+      {id: 6, name: 'Officer 6'},
+      {id: 7, name: 'Officer 7'},
+      {id: 8, name: 'Officer 8'},
+    ]
   },
   getters: {
+    duties: state => state.duties,
+    officers: state => state.officers,
   },
   mutations: {
   },
