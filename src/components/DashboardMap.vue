cbf1f1f72415a0fa7970115dbc6c8030d60d69ca
Shashimii <besasamreginald36@gmail.com>
2025-01-08 00:42:04 +0800

    + dashboard map table

---
+++ src/components/DashboardMap.vue
@@ -0,0 +1,84 @@
+<template>
+    <div ref="table_container"></div>
+</template>
+
+<script>
+export default {
+    name: 'DashboardMap',
+
+    mounted() {
+        this.createTable();
+        const container = this.$refs.table_container;
+        container.appendChild(this.createTable());
+        
+    },
+
+    methods: {
+        createTable() {
+            const table = document.createElement("table");
+            table.id = "my-table";
+            table.className = "centered map-table"
+            
+            const header = table.createTHead();
+
+            const headerRow1 = header.insertRow();
+            headerRow1.className = "map-tr";
+            const headerRow2 = header.insertRow();
+            headerRow2.className = "map-tr";
+
+            const th1 = document.createElement("th");
+            th1.className = "map-th";
+            th1.innerHTML = "<p>Duties / Targets</p>";
+            headerRow1.appendChild(th1);
+
+            const th2 = document.createElement("th");
+            th2.colSpan = this.officers.length;
+            th2.className = "map-th";
+            th2.innerHTML = "<p>Action Officer</p>";
+            headerRow1.appendChild(th2);
+
+            const emptyHeader = document.createElement("th");
+            emptyHeader.className = "map-th";
+            headerRow2.appendChild(emptyHeader);
+
+            this.officers.forEach(officer => {
+                const th = document.createElement("th");
+                th.className = "map-th";
+                th.innerHTML = "<p>" + officer.name + "</p>"
+                headerRow2.appendChild(th);
+            })
+
+            const body = table.createTBody();
+
+            this.duties.forEach((duty, dutyIndex) => {
+                const row = body.insertRow();
+                row.className = "map-tr";
+
+                const dutyCell = document.createElement("th");
+                dutyCell.className = "map-th";
+                dutyCell.innerHTML = "<p>" + duty.duty + "</p>"
+                row.appendChild(dutyCell);
+
+                this.officers.forEach((officer, officerIndex) => {
+                    const dataCell = document.createElement("td");
+                    dataCell.className = "map-td";
+                    dataCell.setAttribute("id", "duty" + dutyIndex + "-" + "officer" + officerIndex)
+                    row.appendChild(dataCell)
+                })
+            })
+
+            return table
+        }
+    },
+
+    computed: {
+        officers() {
+            return this.$store.getters.officers;
+        },
+
+        duties() {
+            return this.$store.getters.duties;
+        }
+    }
+}
+</script>
