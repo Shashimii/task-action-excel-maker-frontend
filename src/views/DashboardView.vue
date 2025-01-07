0d7a983755fb6603a745d7c3e5b3189bda934fd5
Shashimii <besasamreginald36@gmail.com>
2025-01-08 00:42:37 +0800

    + map table

--- src/views/DashboardView.vue
+++ src/views/DashboardView.vue
@@ -3,24 +3,24 @@
     <div class="container">
         <h3>Dashboard</h3>
         <DashboardCards></DashboardCards>
-        <h3>Task Map</h3>
-        <div class="container excel-map">
-            <h6>A Task Map on Excel Format will be Shown Here</h6>
+        <h3>IPCR Tracker</h3>
+        <div>
+            <DashboardMap></DashboardMap>
         </div>
-        
-        <!--Excel Map Should be Here-->
     </div>
 </template>
 
 <script>
 import WebNavbar from '@/components/WebNavbar.vue';
 import DashboardCards from '@/components/DashboardCards.vue';
+import DashboardMap from '@/components/DashboardMap.vue';
 export default {
     name: 'DashboardView',
     components: {
         WebNavbar,
-        DashboardCards
-    }
+        DashboardCards,
+        DashboardMap
+    },
 }
 </script>
 
