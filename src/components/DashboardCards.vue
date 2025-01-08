b2326df22ca750b1c7b234fc5ab5ab1e245d290b
Shashimii <besasamreginald36@gmail.com>
2025-01-09 01:02:24 +0800

    + functional

--- src/components/DashboardCards.vue
+++ src/components/DashboardCards.vue
@@ -4,7 +4,7 @@
             <div class="card teal darken-4">
                 <div class="card-content white-text center">
                     <h5>Total Assigned Duties</h5>
-                    <h4>0</h4>
+                    <h4>{{ assigned.length }}</h4>
                 </div>
             </div>
         </div>
@@ -12,7 +12,7 @@
             <div class="card purple darken-4">
                 <div class="card-content white-text center">
                     <h5>Total Duties</h5>
-                    <h4>0</h4>
+                    <h4>{{ duties.length }}</h4>
                 </div>
             </div>
         </div>
@@ -20,7 +20,7 @@
             <div class="card blue accent-4">
                 <div class="card-content white-text center">
                     <h5>Total Officers</h5>
-                    <h4>0</h4>
+                    <h4>{{ officers.length }}</h4>
                 </div>
             </div>
         </div>
@@ -30,5 +30,19 @@
 <script>
 export default {
     name: 'DashboardCards',
+
+    computed: {
+        officers() {
+            return this.$store.getters.officers
+        },
+
+        duties() {
+            return this.$store.getters.duties
+        },
+
+        assigned() {
+            return this.$store.getters.assigned
+        }
+    }
 }
 </script>
