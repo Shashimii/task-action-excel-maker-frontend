453d2595e6437831f5776eba075858169f5c0f16
Shashimii <besasamreginald36@gmail.com>
2025-01-08 18:40:00 +0800

    % navbar fixed

--- src/components/WebNavbar.vue
+++ src/components/WebNavbar.vue
@@ -1,14 +1,16 @@
 <template>
-    <nav class="black">
-        <div class="nav-wrapper center">
-            <ul id="nav-mobile" class="navbar-items hide-on-med-and-down">
-                <li><router-link to="/dashboard" class="nav-link">Dashboard</router-link></li>
-                <li><router-link to="/assign-duties" class="nav-link">Assign Duties</router-link></li>
-                <li><router-link to="/settings">Settings</router-link></li>
-                <li><router-link to="/">Logout</router-link></li>
-            </ul>
-        </div>
-    </nav>
+    <div class="navbar-fixed">
+        <nav class="black">
+            <div class="nav-wrapper center">
+                <ul id="nav-mobile" class="navbar-items hide-on-med-and-down">
+                    <li><router-link to="/dashboard" class="nav-link">Dashboard</router-link></li>
+                    <li><router-link to="/assign-duties" class="nav-link">Assign Duties</router-link></li>
+                    <li><router-link to="/settings">Settings</router-link></li>
+                    <li><router-link to="/">Logout</router-link></li>
+                </ul>
+            </div>
+        </nav>
+    </div>
 </template>
 
 <script>
