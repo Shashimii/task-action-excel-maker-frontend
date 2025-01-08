c1b3a500b83670373cca3107b724dda89cbe1ef4
Shashimii <besasamreginald36@gmail.com>
2025-01-08 18:41:15 +0800

    + added no duty prompt

--- src/views/AssignDutiesView.vue
+++ src/views/AssignDutiesView.vue
@@ -1,11 +1,22 @@
 <template>
     <WebNavbar></WebNavbar>
+    
     <div class="container">
+        
         <h3 class="center">IPCR Tracker</h3>
         <DashboardMap></DashboardMap>
+
         <h3>Duties Assigned</h3>
-        <AssignedDutiesList></AssignedDutiesList>
+        <div v-if="this.assigned.length != '0'">
+            <AssignedDutiesList></AssignedDutiesList>
+        </div>
+        <div v-else class="no-prompt">
+            <p class="no-prompt-primary-text">Duties Assigned to Officers will be shown here.</p>
+            <p class="no-prompt-secondary-text">Assign duty to officers using the Floating Button on the bottom right of the screen.</p>
+        </div>
+
     </div>
+
     <AssignDutiesModal></AssignDutiesModal>
     <div class="fixed-action-btn">
         <a class="btn-floating btn-large waves-effect waves-light green accent-4 modal-trigger" href="#AssignDutiesModal">
@@ -36,6 +47,12 @@ export default {
 
             const ModalElems = document.querySelectorAll('.modal');
             M.Modal.init(ModalElems, {}); 
+        }
+    },
+
+    computed: {
+        assigned() {
+            return this.$store.getters.assigned;
         }
     },
 
