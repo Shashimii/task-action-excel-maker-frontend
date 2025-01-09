752be5ec33dcd6426bea23d7bd84d1d4c1f53765
Shashimii <besasamreginald36@gmail.com>
2025-01-09 17:52:40 +0800

    + Modal for Add New

--- src/components/SettingsAddPanel.vue
+++ src/components/SettingsAddPanel.vue
@@ -3,16 +3,72 @@
         <div class="card-content">
             <h6>Add New</h6>
             <ul class="add-panel-items">
-                <li><button class="btn-small waves-effect waves-light green darken-4 btn-block">Duty</button></li>
-                <li><button class="btn-small waves-effect waves-light green darken-4 btn-block">Officer</button></li>
+                <li><a class="btn-small waves-effect waves-light green darken-4 btn-block modal-trigger" href="#AddDutiesModal">Duty</a></li>
+                <li><a class="btn-small waves-effect waves-light green darken-4 btn-block modal-trigger" href="#AddOfficerModal">Officer</a></li>
             </ul>
         </div>
+    </div>
+
+
+
+    <div id="AddDutiesModal" class="modal">
+        <form @submit.prevent="submitDuties">
+            <div class="modal-content">
+                <h4>Add Duty</h4>
+                <div class="input-field col s12">
+                    <input type="text" placeholder="Duty Title" class="validate" id="dutyTitle">
+                    <label for="dutyTitle">Title</label>
+                </div>
+            </div>
+            <div class="modal-footer">
+                <a @click.prevent="cancel" class="modal-close waves-effect waves-green btn-flat">Cancel</a>
+                <button class="btn waves-effect waves-light green acccent-4" type="submit" name="action">
+                    Add Duty
+                    <i class="material-icons right">send</i>
+                </button>
+            </div>
+        </form>
+    </div>
+
+    <div id="AddOfficerModal" class="modal">
+        <form @submit.prevent="submitOfficer">
+            <div class="modal-content">
+                <h4>Add Officer</h4>
+                <div class="input-field col s12">
+                    <input type="text" placeholder="Officer Name" class="validate" id="officerName">
+                    <label for="officerName">Name</label>
+                </div>
+            </div>
+            <div class="modal-footer">
+                <a @click.prevent="cancel" class="modal-close waves-effect waves-green btn-flat">Cancel</a>
+                <button class="btn waves-effect waves-light green acccent-4" type="submit" name="action">
+                    Add Officer
+                    <i class="material-icons right">send</i>
+                </button>
+            </div>
+        </form>
     </div>
 </template>
 
 <script>
 export default {
     name: 'AddPanel',
+
+    methods: {
+        initalizeMaterializeCSS() {
+            const ModalElems = document.querySelectorAll('.modal');
+            M.Modal.init(ModalElems, {});
+            M.updateTextFields() 
+        }
+    },
+
+    mounted() {
+        this.initalizeMaterializeCSS();
+    },
+
+    updated() {
+        this.initalizeMaterializeCSS();
+    }
 }
 </script>
 
@@ -22,4 +78,14 @@ export default {
     flex-direction: column;
     gap: 1rem;
 }
+
+.modal-content {
+    display: flex;
+    flex-direction: column;
+}
+
+.validation {
+    color: red;
+}
+
 </style>
