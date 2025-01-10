d4ecb458f95dc5876f856e0448375f1d9b2384dd
Shashimii <besasamreginald36@gmail.com>
2025-01-10 14:13:02 +0800

    % whitespace adjustment

--- src/views/DashboardView.vue
+++ src/views/DashboardView.vue
@@ -14,6 +14,7 @@
 import WebNavbar from '@/components/WebNavbar.vue';
 import DashboardCards from '@/components/DashboardCards.vue';
 import DashboardMap from '@/components/DashboardMap.vue';
+
 export default {
     name: 'DashboardView',
     components: {
