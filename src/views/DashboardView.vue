e00b389095a5630fca9da0ec0749e29f452a3f99
Shashimii <besasamreginald36@gmail.com>
2025-01-14 16:31:29 +0800

    % removed api test data

--- src/views/DashboardView.vue
+++ src/views/DashboardView.vue
@@ -22,18 +22,6 @@ export default {
         DashboardCards,
         DashboardMap
     },
-
-    computed: {
-        apiTestData() {
-            return this.$store.getters.apiTestData;
-        }
-    },
-
-    mounted() {
-        this.$store.dispatch('apiTest')
-        console.log(this.apiTestData)
-    }
-    
 }
 </script>
 
