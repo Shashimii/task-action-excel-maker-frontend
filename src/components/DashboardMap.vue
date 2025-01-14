e17a5731b5a00dff8e602ca4a693ae2be5a88a15
Shashimii <besasamreginald36@gmail.com>
2025-01-14 16:31:09 +0800

    % use vue template instead of direct DOM manipulation for creating table

--- src/components/DashboardMap.vue
+++ src/components/DashboardMap.vue
@@ -1,6 +1,35 @@
 <template>
-    <div v-if="this.officers.length != 0 && this.duties.length != 0">
-        <div ref="table_container" class="map-table-container"></div>
+    <div v-if="hasData">
+        <div ref="table_container" class="map-table-container">
+            <table id="mapTable" class="centered map-table">
+                <thead>
+                    <tr class="map-tr">
+                        <th class="map-th">
+                            <p>Duties / Targets</p>
+                        </th>
+                        <th :colspan="officers.length" class="map-th">
+                            <p>Action Officer</p>
+                        </th>
+                    </tr>
+                    <tr class="map-tr">
+                        <th class="map-th">
+                        </th>
+                        <th v-for="(officer, officerIndex) in officers" :key="officerIndex" class="map-th">
+                            <p>{{ officer.name }}</p>
+                        </th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr v-for="(duty, dutyIndex) in duties" :key="dutyIndex" class="map-tr">
+                        <th class="map-th">
+                            <p>{{ duty.duty }}</p>
+                        </th>
+                        <td v-for="(officer, officerIndex) in officers" :key="officerIndex" class="map-td" :id="'duty' + dutyIndex + '-' + 'officer' + officerIndex">
+                        </td>
+                    </tr> 
+                </tbody>
+            </table>
+        </div>
     </div>
     <div v-else class="no-prompt">
         <p class="no-prompt-primary-text">Not Enough Duties and Officer to show IPCR Tracker.</p>
@@ -13,71 +42,12 @@ export default {
     name: 'DashboardMap',
 
     methods: {
-        createTable() {
-            const table = document.createElement("table");
-            table.id = "mapTable";
-            table.className = "centered map-table"
-            
-            const header = table.createTHead();
-
-            const headerRow1 = header.insertRow();
-            headerRow1.className = "map-tr";
-            const headerRow2 = header.insertRow();
-            headerRow2.className = "map-tr";
-
-            const th1 = document.createElement("th");
-            th1.className = "map-th";
-            th1.innerHTML = "<p>Duties / Targets</p>";
-            headerRow1.appendChild(th1);
-
-            const th2 = document.createElement("th");
-            th2.colSpan = this.officers.length;
-            th2.className = "map-th";
-            th2.innerHTML = "<p>Action Officer</p>";
-            headerRow1.appendChild(th2);
-
-            const emptyHeader = document.createElement("th");
-            emptyHeader.className = "map-th";
-            headerRow2.appendChild(emptyHeader);
-
-            this.officers.forEach(officer => {
-                const th = document.createElement("th");
-                th.className = "map-th";
-                th.innerHTML = "<p>" + officer.name + "</p>"
-                headerRow2.appendChild(th);
-            })
-
-            const body = table.createTBody();
-
-            this.duties.forEach((duty, dutyIndex) => {
-                const row = body.insertRow();
-                row.className = "map-tr";
-
-                const dutyCell = document.createElement("th");
-                dutyCell.className = "map-th";
-                dutyCell.innerHTML = "<p>" + duty.duty + "</p>"
-                row.appendChild(dutyCell);
-
-                this.officers.forEach((officer, officerIndex) => {
-                    const dataCell = document.createElement("td");
-                    dataCell.className = "map-td";
-                    dataCell.setAttribute("id", "duty" + dutyIndex + "-" + "officer" + officerIndex)
-                    row.appendChild(dataCell)
-                })
-            })
-
-            return table
-        },
-
         assignDuty() {
             const assignedList = this.assigned;
-
             const cells = document.querySelectorAll('#mapTable td.map-td');
             cells.forEach(cell => {
                 cell.innerHTML = ''
             })
-
-            console.log(cells);
 
             assignedList.forEach((assign) => {
                 const cellIndex = assign.dutyIndex + '-' + assign.officerIndex;
@@ -88,9 +58,7 @@ export default {
                 } else {
                     console.error('unable to find cell');
                 }
-                
             })
-            
         }
     },
 
@@ -106,6 +74,11 @@ export default {
         assigned() {
             return this.$store.getters.assigned;
         },
+
+        // checks if the data is loaded from the api
+        hasData() {
+            return this.officers.length !== 0 && this.duties.length !== 0;
+        }
     },
 
     watch: {
@@ -113,18 +86,15 @@ export default {
             handler() {
                 this.assignDuty();
             },
-            deep: true
-        },
+        }
     },
 
     mounted() {
-        if (this.officers.length != 0 && this.duties.length != 0) {
-            this.createTable();
-            const container = this.$refs.table_container;
-            container.appendChild(this.createTable());
-            this.assignDuty();
-        }
-    }
+        // loads the data
+        this.$store.dispatch('requestDutiesData') // duties
+        this.$store.dispatch('requestOfficersData') // officers
+        this.$store.dispatch('requestAssignedData') // assigned
+    },
 }
 </script>
 
