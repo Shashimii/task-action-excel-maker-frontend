21c1b05936e9f46dfc54d34098970a4dc3d139b2
Shashimii <besasamreginald36@gmail.com>
2025-01-14 16:29:51 +0800

    + added id and handle api data post then reloads data

--- src/components/AssignDutiesModal.vue
+++ src/components/AssignDutiesModal.vue
@@ -43,6 +43,7 @@
 </template>
 
 <script>
+import axios from 'axios';
 export default {
     name: 'AssignDutiesModal',
 
@@ -55,7 +56,9 @@ export default {
                 date: '',
                 code: '',
                 officerIndex: '',
-                dutyIndex: ''                
+                dutyIndex: '',
+                officerId: null,
+                dutyId: null                
                 
             },
             // validation data
@@ -64,17 +67,20 @@ export default {
                 duty: '',
                 date: '',
                 code: ''
-            }
+            },
         }
     },
 
     methods: {
+        // handle submission
         submit() {
             if(this.validateSubmit()) {
                 const selectionOfficer = this.$refs.selectionOfficer;
                 const selectionDuty = this.$refs.selectionDuty;
                 const selectedOfficer = selectionOfficer.options[selectionOfficer.selectedIndex];
                 const selectedDuty = selectionDuty.options[selectionDuty.selectedIndex];
+                const extractedOfficerIndex = parseInt(this.assignData.officerIndex.replace(/\D+/g, ""), 10);
+                const extractedDutyIndex = parseInt(this.assignData.dutyIndex.replace(/\D+/g, ""), 10);
                 const modal = M.Modal.getInstance(document.getElementById('AssignDutiesModal'));
                 const inputDate = document.getElementById('inputDate');
 
@@ -85,9 +91,13 @@ export default {
                 if (selectedDuty) {
                     this.assignData.duty = selectedDuty.text;
                 }
+
+                // set the officer and duty id
+                this.assignData.officerId = this.officers[extractedOfficerIndex].id;
+                this.assignData.dutyId = this.duties[extractedDutyIndex].id;
                 
                 modal.close(); // close the modal
-                this.$store.dispatch('assignDuty', this.assignData); // store the submitted data
+                this.postAssigned(this.assignData) // post assigned duty data -> database
 
                 M.toast({
                     html: '<p class="toast-text">Duty Assigned Successfully.<p>',
@@ -101,14 +111,15 @@ export default {
                     date: '',
                     code: '',
                     officerIndex: '',
-                    dutyIndex: '' 
+                    dutyIndex: '',
+                    officerId: '',
+                    dutyId: ''    
                 }
                 inputDate.value = ''; // clear the datepicker
-
-                console.table(this.assigned)
             }
         },
 
+        // handle cancelation
         cancel() {
             const inputDate = document.getElementById('inputDate');
 
@@ -119,7 +130,9 @@ export default {
                 date: '',
                 code: '',
                 officerIndex: '',
-                dutyIndex: '' 
+                dutyIndex: '',
+                officerId: '',
+                dutyId: ''  
             }
             inputDate.value = ''; // clear the date picker
 
@@ -132,6 +145,7 @@ export default {
             }
         },
 
+        // handle validation on submit
         validateSubmit() {
             // validiation message holder
             this.validation = {
@@ -176,6 +190,33 @@ export default {
             return isValid;
         },
 
+        // post the submission data -> database
+        async postAssigned(assignData) {
+            try {
+                const response = await axios.post('http://127.0.0.1:8000/api/addAssigned', {
+                    officer_Id: assignData.officerId,
+                    duty_Id: assignData.dutyId,
+                    officer: assignData.officer,
+                    duty: assignData.duty,
+                    date: assignData.date,
+                    code: assignData.code,
+                    officerIndex: assignData.officerIndex,
+                    dutyIndex: assignData.dutyIndex
+                })
+
+                if (response.status === 201) {
+                    this.reloadAssigned();
+                }
+            } catch (error) {
+                console.log('unable to post assigned: ', error.response.data)
+            }
+        },
+
+        reloadAssigned() {
+            this.$store.dispatch('requestAssignedData') // reload assigned data
+        },
+
+        // materializeCSS
         initalizeMaterializeCSS() {
             // select
             const selectElems = document.querySelectorAll('select');
@@ -194,24 +235,17 @@ export default {
     },
 
     computed: {
-        // get data
         officers() {
             return this.$store.getters.officers;
         },
-
         duties() {
             return this.$store.getters.duties;
-        },
-
-        assigned() {
-            return this.$store.getters.assigned;
         }
     },
 
     mounted() {
         this.initalizeMaterializeCSS();
     },
-
     updated() {
         this.initalizeMaterializeCSS();
     },
