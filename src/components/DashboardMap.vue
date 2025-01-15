ff783690d2b973e0a4cbb13285d1fd346c5d51b3
Shashimii <besasamreginald36@gmail.com>
2025-01-15 18:43:57 +0800

    + ExcelJS Export Implementation (Unoptimized for VueJS)

--- src/components/DashboardMap.vue
+++ src/components/DashboardMap.vue
@@ -30,6 +30,10 @@
                 </tbody>
             </table>
         </div>
+
+        <div class="container center">
+            <button class="btn green accent-4" @click="this.excelExport()"><i class="material-icons left">file_download</i>Download Table as Excel File</button>
+        </div>
     </div>
     <div v-else class="no-prompt">
         <p class="no-prompt-primary-text">Not Enough Duties and Officer to show IPCR Tracker.</p>
@@ -38,6 +42,7 @@
 </template>
 
 <script>
+import ExcelJS from 'exceljs';
 export default {
     name: 'DashboardMap',
 
@@ -59,9 +64,159 @@ export default {
                     console.error('unable to find cell');
                 }
             })
+        },
+        
+        // this excel export method is still not optimized and created on vanila javascript
+        excelExport() {
+            let workbook = new ExcelJS.Workbook(); // creare a new worksheet using ExcelJS
+            let worksheet = workbook.addWorksheet("Sheet1") // add sheet named "Sheet 1"
+            let table = document.getElementById("mapTable") // Html Table
+
+            // make the excel table
+            let tableData = [] // table data 2d array
+            const rowCount = table.rows.length; // count the number of rows needed
+            
+            // create the row
+            const htmlTableRowToArray = (htmlTableRow) => {
+                return Array.from(htmlTableRow.cells).map(cell => {
+                    if (cell.textContent != '' && cell.querySelector("p")) { // check if there is a text on the cell "<p>"
+                        return Array.from(cell.querySelectorAll("p")).map(p => p.textContent.trim() + "\n").join(" "); // format the cell texts accordingly
+                    } else {
+                        return cell.textContent.trim(); // trim whitespaces only for blank cells
+                    }
+                })
+            }
+
+            // create the 2d array
+            for (let i = 0; i < rowCount; i++) {
+                const htmlTableRow = htmlTableRowToArray(table.rows[i]); // create the rows
+                tableData.push(htmlTableRow) // push the rows to 2d array
+            }
+
+            // console.table(tableData) // debug the table
+
+            // functions
+
+            // format selcted text color using rich text
+            const formatTextColorGreen = (text) => {
+                const regex = /(\([^)]+\))/g; // match to texts like "(text)"
+                const textParts = text.split(regex); // split text into parts as needed in richText
+
+                // map and transform into new array for richText
+                return textParts.map(part => {
+                    // check for matching texts on every part
+                    if (regex.test(part)) {
+                        // apply the green color on matching parts
+                        return {font: {color: {argb: '008000'} }, text: part};
+                    } else {
+                        // apply nothing
+                        return {text: part};
+                    }
+                });
+            }
+
+            // count the line breaks for cell height calculation
+            const countLineBreaks = (cell) => {
+                if (typeof cell !== 'string') return 0;
+                const matches = cell.match(/\n/g);
+                
+                return matches ? matches.length : 0;
+            }
+
+            // create the excel table
+            tableData.forEach((row, rowIndex) => {
+                let worksheetRow = worksheet.addRow(row); // add tableData as rows
+                let autoHeight = 0; // initalize height
+
+                row.forEach((cellData, cellIndex) => {
+                    
+                    // determine the exact height of the row
+                    let height = 100 + countLineBreaks(cellData) * 10; // compute the height
+                    if (height > autoHeight) { // if height > current auto height
+                        autoHeight = height; // set new auto height
+                    }
+
+                    // height
+                    if (rowIndex <= 1) {
+                        worksheet.getRow(rowIndex + 1).height = 40; // fixed height for the headers
+                    } else {
+                        worksheet.getRow(rowIndex + 1).height = autoHeight; // calculated height for the data rows
+                    }
+
+                    // width
+                    worksheet.getColumn(cellIndex + 1).width = 30;
+
+                    // apply cell styles
+                    let cell = worksheetRow.getCell(cellIndex + 1);
+
+                    // font color for codes
+                    if (cellIndex !== 0 && typeof cellData === 'string') {
+                        cell.value = {
+                            richText: formatTextColorGreen(cellData)
+                        }
+                    } else {
+                        cell.value = cellData;
+                    }
+
+                    // border
+                    cell.border = {
+                        top: { style: "medium", color: { argb: "000000" } },
+                        left: { style: "medium", color: { argb: "000000" } },
+                        bottom: { style: "medium", color: { argb: "000000" } },
+                        right: { style: "medium", color: { argb: "000000" } },
+                    }
+
+                    // background color
+                    if (rowIndex <= 1) {
+                        cell.fill = {
+                            type: "pattern",
+                            pattern: "solid",
+                            fgColor: { argb: "F4F4F4" },
+                        };
+                        
+                        cell.font = {
+                            bold: true,
+                            size: 14
+                        };
+                    } else if (rowIndex % 2 === 1 && rowIndex > 2 && cellIndex != 0) {
+                        cell.fill = {
+                            type: "pattern",
+                            pattern: "solid",
+                            fgColor: { argb: "F4F4F4" },
+                        };
+                    } else if (cellIndex === 0) {
+                        cell.fill = {
+                            type: "pattern",
+                            pattern: "solid",
+                            fgColor: { argb: "E0E0E0" },
+                        };
+
+                        cell.font = {
+                            bold: true,
+                            size: 14
+                        };
+                    }
+
+                    // text wrapping and alignment
+                    cell.alignment = { vertical: "middle", horizontal: "center", wrapText: true };
+                });
+            });
+
+            // cell merging for the header
+            worksheet.mergeCells("B1:H1");
+
+            // write the file then download
+            workbook.xlsx.writeBuffer().then((buffer) => {
+                let blob = new Blob([buffer], { type: "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet" });
+                let link = document.createElement("a");
+                link.href = URL.createObjectURL(blob);
+                link.download = "IPCR.xlsx";
+                link.click();
+            });
+
         }
     },
-
+    
     computed: {
         officers() {
             return this.$store.getters.officers;
